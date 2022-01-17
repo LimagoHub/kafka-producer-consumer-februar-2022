190196842063aeebed5146d9d60c57e29b03656d
Jo Wagner <jowagner@limago.de>
2022-01-17 09:55:47 +0100

    Initial commit

---
+++ p01-simple/Producer/src/main/java/de/limago/SimpleKafkaProducer.java
@@ -0,0 +1,48 @@
+package de.limago;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Map;
+
+public class SimpleKafkaProducer {
+
+    private static final String TOPIC = "SimpleTopic";
+
+
+    public void run() {
+
+        try {
+            var producer = createKafkaProducer();
+
+
+            for (var i = 0; i < 5; i ++) {
+                var record = createProducerRecord(TOPIC, "Hallo " + i,"Hallo " + i);
+                producer.send(record).get();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ProducerRecord<String, String> createProducerRecord(String topic, String key, String value) {
+        return new ProducerRecord<>(topic, key, value);
+    }
+
+    private Producer<String, String> createKafkaProducer() {
+        return new KafkaProducer<>(createProperties());
+    }
+
+
+    private Map createProperties () {
+        return  Map.of(
+                "bootstrap.servers", "localhost:9092",
+                //"schema.registry.url", "http://localhost:8081",
+                "acks", "all",
+                "retries", 0,
+                "value.serializer", "org.apache.kafka.common.serialization.StringSerializer",
+                "key.serializer", "org.apache.kafka.common.serialization.StringSerializer"
+        );
+    }
+}
